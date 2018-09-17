205b623a1d828d375d8fba332c27dc54ee767ba6
nebevets <steveben@exmsft.com>
2018-09-17 14:34:06 -0700

    input challenge finished

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,14 @@ import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import DemoForm from './demo_form';
 
+const fakeUser = {
+    email: 'some_email@test.com'
+}
+
 const App = () => (
     <div>
         <div className="app">
-            <DemoForm />
+            <DemoForm email={fakeUser.email}/>
         </div>
     </div>
 );
--- src/components/demo_form.js
+++ src/components/demo_form.js
@@ -4,32 +4,45 @@ class DemoForm extends Component{
     constructor(props){
         super(props);
         this.state = {
-            email: '',
+            email: props.email || '',
+            name: '',
             password: ''
         };
-        this.handleChange = this.handleChange.bind(this);
+        //this.handleChange = this.handleChange.bind(this);
         //this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleSubmit(event){
+    handleSubmit = (event) => {
         event.preventDefault();
         console.log('form submitted', this.state);
+        this.setState({
+            email: '',
+            name: '',
+            password: ''
+        })
     }
-    handleChange(event){
-        console.log('value, bud', event.target.value);
-        console.log('name, bud', event.target.name);
+    handleChange = (event) => {
+        //console.log('value, bud', event.target.value);
+        //console.log('name, bud', event.target.name);
         //console.dir(event.target);
-        switch(event.target.name){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        /*
+        switch (event.target.name){
             case 'email':
                 this.setState({email: event.target.value});
+                break;
+            case 'name':
+                this.setState({name: event.target.value});
                 break;
             case 'password':
                 this.setState({password: event.target.value});
                 break;
-        }
+        }*/
     }
     render(){
         return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
+            <form onSubmit={this.handleSubmit}>
                 <h1>demo form</h1>
                 <div>
                     <label>email</label>
@@ -38,6 +51,16 @@ class DemoForm extends Component{
                         value={this.state.email}
                         onChange={this.handleChange}
                         name="email"
+                        autoComplete="off"
+                    />
+                </div>
+                <div>
+                    <label>name</label>
+                    <input
+                        type="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        name="name"
                         autoComplete="off"
                     />
                 </div>
